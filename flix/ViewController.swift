0857e16d9f52307374382c9675b726f0efbc3283
leolanggeng <langgeng@cpp.edu>
2022-02-24 16:46:00 -0800

    Initial Commit

---
+++ flix/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  flix
+//
+//  Created by Leonardo Langgeng on 2/24/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
